1321abcdc8cfdb592d6cb050bdaf50a21e2e1eef
Zuhaib Alam <zuhaibalamhunzai188@gmail.com>
2025-12-02 15:33:19 +0500

    feat: add new files to export

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,8 +3,21 @@ export { Button } from "./components/Button";
 export { Card, CardHeader, CardContent } from "./components/Card";
 
 // Themes
-export { defaultTheme, darkTheme } from "../src/themes/defaults";
-export type { Theme } from "../src/themes/defaults";
+export { defaultTheme, darkTheme } from "./themes/defaults";
+export { userLightTheme, userDarkTheme } from "./themes/user-theme";
+export type { Theme } from "./themes/defaults";
 
 // Contexts
-export { ThemeProvider, useTheme } from "../src/contexts/themeContext";
+export { ThemeProvider, useTheme } from "./contexts/themeContext";
+
+// Theme Utilities
+export {
+  setUserTheme,
+  getActiveTheme,
+  isUsingCustomTheme,
+} from "./utils/theme-detector";
+
+export { createTheme, createThemePair } from "./utils/theme-builder";
+
+// Re-export everything for easy imports
+export * as ThemeUtils from "./utils/theme-builder";
