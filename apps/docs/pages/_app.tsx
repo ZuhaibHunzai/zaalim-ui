1cdcfd4fbad7dadc0e905e7540e3503a08762242
Zuhaib Alam <zuhaibalamhunzai188@gmail.com>
2025-12-02 15:28:42 +0500

    fix: removed unused code

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -2,8 +2,5 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // Check if we're on a docs page
-  const isDocsPage = pageProps?.isDocsPage ?? false;
-
   return <Component {...pageProps} />;
 }
