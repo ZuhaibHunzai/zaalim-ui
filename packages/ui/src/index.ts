428ce5ab6ab537876f641a3b0843967259fcefaa
Zuhaib Alam <zuhaibalamhunzai188@gmail.com>
2025-12-04 01:53:43 +0500

    chore: update barrel exports for new components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,3 @@
-// Components
-export { Button } from "./components/Button";
-export { Card, CardHeader, CardContent, CardFooter } from "./components/Card";
-
 // Themes
 export { defaultTheme, darkTheme } from "./themes/defaults";
 export { userLightTheme, userDarkTheme } from "./themes/user-theme";
@@ -18,6 +14,25 @@ export {
 } from "./utils/theme-detector";
 
 export { createTheme, createThemePair } from "./utils/theme-builder";
+
+// Components
+export { Button } from "./components/Button";
+export { Card, CardHeader, CardContent, CardFooter } from "./components/Card";
+export {
+  Typography,
+  Heading,
+  Display,
+  Title,
+  Subtitle,
+  Paragraph,
+  SmallText,
+  Label,
+  Caption,
+  Code,
+} from "./components/Typograpgy";
+export { Input, Textarea } from "./components/forms/Input";
+export { Select } from "./components/forms/select";
+export { Checkbox, CheckboxGroup } from "./components/forms/checkbox";
 
 // Re-export everything for easy imports
 export * as ThemeUtils from "./utils/theme-builder";
