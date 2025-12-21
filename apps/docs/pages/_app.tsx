6f8e81ce95178ae5df57bcca793bbf6429b03fde
Zuhaib Alam <zuhaibalamhunzai188@gmail.com>
2025-12-21 22:59:39 +0500

    docs (fix): removed unused imports

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "zaalim-ui";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useTheme } from "zaalim-ui";
 import { defaultTheme } from "../components/theme/default";
 
 export default function App({ Component, pageProps }: AppProps) {
